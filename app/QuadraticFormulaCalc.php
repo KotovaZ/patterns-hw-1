da85646cab3a55f7f2d786d577a5966d676fe55f
Basil Tatarnikov <tatvasily@gmail.com>
2023-03-15 10:57:40 +0800

    add QuadraticFormulaCalc class

---
+++ app/QuadraticFormulaCalc.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+class QuadraticFormulaCalc
+{
+    /**
+     * Method solve
+     *
+     * @param float $a
+     * @param float $b
+     * @param float $c
+     *
+     * @return float[]
+     */
+    public function solve(float $a, float $b, float $c = PHP_FLOAT_EPSILON): array
+    {
+    }
+}
