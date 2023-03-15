a0adb484bd89e1140e5929d68ced6facd749d4cf
Basil Tatarnikov <tatvasily@gmail.com>
2023-03-15 11:41:05 +0800

    feat: validate leading coefficient value

--- app/QuadraticFormulaCalc.php
+++ app/QuadraticFormulaCalc.php
@@ -2,18 +2,23 @@
 
 namespace App;
 
+use App\Exceptions\InvalidArgumentException;
+
 class QuadraticFormulaCalc
 {
     /**
-     * Method solve
-     *
      * @param float $a
      * @param float $b
      * @param float $c
      *
      * @return float[]
      */
-    public function solve(float $a, float $b, float $c = PHP_FLOAT_EPSILON): array
+    public function solve(float $a, float $b, float $c = 0): array
     {
+        if (abs($a) < PHP_FLOAT_EPSILON) {
+            throw new InvalidArgumentException('Leading coefficient must be greater than 0');
+        }
+
+        return [];
     }
 }
