f2dac5a11a66831ff97a28ed0b84fd3f54327506
Basil Tatarnikov <tatvasily@gmail.com>
2023-03-24 10:22:33 +0800

    feat(tests): added test for special numbers

--- tests/QuadraticFormulaCalcTest.php
+++ tests/QuadraticFormulaCalcTest.php
@@ -4,6 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . 'vendor/autoload.php';
 
 use App\Exceptions\InvalidArgumentException;
 use App\QuadraticFormulaCalc;
+use InvalidArgumentException as GlobalInvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 final class QuadraticFormulaCalcTest extends TestCase
@@ -41,5 +42,23 @@ final class QuadraticFormulaCalcTest extends TestCase
         $roots = $this->calculator->solve(1, 2, 1);
         $this->assertCount(1, $roots);
         $this->assertEquals(-1, $roots[0]);
+    }
+
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testSolveThrowsExceptionForSpecialNumbers()
+    {
+        foreach ([NAN, INF] as $specialValue) {
+            for ($i = 0; $i < 3; $i++) {
+                $arguments = [1, 1, 2, $i => $specialValue];
+                try {
+                    call_user_func_array([$this->calculator, 'solve'], $arguments);
+                    $errorMessage = sprintf("Argument %d: special number %s does not throw an exception", $i + 1, $specialValue);
+                    $this->fail($errorMessage);
+                } catch (\InvalidArgumentException $e) {
+                }
+            }
+        }
     }
 }
